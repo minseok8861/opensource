6614204c8ff1972c741ccf597aaee54c702bde0e
GitJangminseo <mingi7498@naver.com>
2022-12-08 00:04:24 +0900

    recommit

---
+++ GameController.java
@@ -0,0 +1,48 @@
+public class GameController {
+    
+    private GameBoard board;
+    private BoardWriter writer;
+    private Reader reader;
+    
+    public GameController(GameBoard b, BoardWriter w, Reader r) {
+        board = b;
+        writer = w;
+        reader = r;
+    }
+    
+    public void play() {
+        Cell[][] square = board.square();
+        int opened_cell = 16;
+        while(opened_cell != 0) {
+        	int cell1 = reader.readInput();
+        	cell1--;
+        	int row1 = cell1/4;
+        	int col1 = cell1%4;
+        	square[row1][col1].reveal();
+        	writer.repaint();
+        	int cell2 = reader.readInput();
+        	while (cell2 == cell1)
+        		cell2 = reader.readInput();
+        	cell2--;
+        	int row2 = cell2/4;
+        	int col2 = cell2%4;
+        	square[row2][col2].reveal();
+        	writer.repaint();
+        	delay(2000);
+        	if(square[row1][col1].color() == square[row2][col2].color())
+        		opened_cell -= 2;
+        	else {
+        		square[row1][col1].hide();
+        		square[row2][col2].hide();
+        		writer.repaint();
+        	}
+        }
+    }
+    
+    /** delay - how_long millisecond 동안 실행 정지  */
+    private void delay(int how_long) { 
+        try { Thread.sleep(how_long); }
+        catch (InterruptedException e) { }
+    }
+
+}
---
+++ MatchingGame.java
@@ -0,0 +1,12 @@
+package MatchingGame;
+
+public class MatchingGame {
+
+    public static void main(String[] args) {
+        GameBoard board = new GameBoard();
+        BoardWriter writer = new BoardWriter(board);
+        Reader reader = new Reader();
+        new GameController(board,writer,reader).play();
+    }
+
+}
